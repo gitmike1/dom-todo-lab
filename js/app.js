7f336ac0efb8ab8191678fb7d921b27182e557c3
Jon Michael <jmvlu27@hotmail.com>
2022-02-03 08:54:54 -0500

    create cached element references

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
 console.log('sanitycheck')
+const inpElement = document.getElementById('inp')
+const btnElement = document.getElementById('submit-button')
+const ulElement = document.getElementById('todo-list')
