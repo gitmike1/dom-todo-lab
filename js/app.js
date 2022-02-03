7bb8323086e4d782a159a5d23df9f9207200d740
Jon Michael <jmvlu27@hotmail.com>
2022-02-03 17:54:44 -0500

    solve number 6 add li

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ console.log('sanitycheck')
 const inpElement = document.getElementById('inp')
 const btnElement = document.getElementById('submit-button')
 const ulElement = document.getElementById('todo-list')
+
+btnElement.addEventListener('click', function(evt){
+  if(inpElement !== ""){
+  const newLi = document.createElement('li')
+  newLi.textContent = inpElement.value
+  document.getElementById('todo-list').appendChild(newLi)
+  }
+  inpElement.value = " "
+})
